53b6cb1722e14c52214591e50f632f341851bd5a
Ayush Soni <ayushsoni626@gmail.com>
2019-06-20 01:18:26 +0530

    Searching in list and remove it
    Taking input from the user and stored it in a list the take one more input and then if found that element in a list then remove it.

---
+++ SearchAndRemove.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+a=[]
+n=int(input("Enter the no. of input "))
+for i in range(0,n):
+    w=int(input())
+    a.append(w)
+print(a) 
+q=int(input("Enter the no to search in the list "))
+if q in a :
+    a.remove(q)
+else :
+    print("not found") 
+print(a)
